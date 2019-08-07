05c503f46b720529702ee5f5e41df28e712df816
surojitp <rocksuro007@gmail.com>
2019-08-07 21:01:08 +0530

    changes before marge

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Surojit',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 5, 2018'
-    },
-    {
-        'author': 'Bikash',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 10, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Surojit',
+#         'title': 'Blog post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 5, 2018'
+#     },
+#     {
+#         'author': 'Bikash',
+#         'title': 'Blog post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 10, 2018'
+#     }
+# ]
 
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts
     }
